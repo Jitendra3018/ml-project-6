ab9df917e35fbcd83b100f1c625c12652e1cbf11
Jitendra Garg <58606315+Jitendra3018@users.noreply.github.com>
2022-05-26 00:53:07 +0530

    Initial Files Added

---
+++ buildTimePredictor.py
@@ -0,0 +1,230 @@
+# imports
+import math
+import pandas as pd
+from IPython import display
+import tensorflow as tf
+from matplotlib import pyplot as plt
+import numpy as np
+from sklearn import metrics
+from tensorflow.python.data import Dataset
+from tensorflow.keras import layers
+
+
+def construct_feature_columns():
+    bucketized_prefix = tf.feature_column.categorical_column_with_vocabulary_list(
+        key="prefix",
+        vocabulary_list=["bazelci/", "examples/", "scripts/", "site/", "src/conditions",
+                         "src/java_tools", "src/main", "src/test", "src/tools", "third_party/", "tools/"],
+        num_oov_buckets=8)
+    print(bucketized_prefix)
+
+    bucketized_type = tf.feature_column.categorical_column_with_vocabulary_list(
+        key="type",
+        vocabulary_list=["JAVA", "C/C++", "Starlark", "python", "HTML/CSS/JS"], num_oov_buckets=5)
+    print(bucketized_type)
+
+    prefix_x_type = tf.feature_column.crossed_column(
+        set([bucketized_prefix, bucketized_type]), hash_bucket_size=1000)
+
+    feature_columns = set([
+        bucketized_prefix,
+        bucketized_type,
+        prefix_x_type,
+    ])
+
+    return feature_columns
+
+
+def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
+    # convert pandas data into a dict of np arrays
+    features = {key: np.array(value) for key, value in dict(features).items()}
+
+    # construct a dataset, and configure batching/repeating
+    ds = Dataset.from_tensor_slices((features, targets))  # beware of limits
+    ds = ds.batch(batch_size).repeat(num_epochs)
+
+    # shuffle the data, if specified
+    if shuffle:
+        ds = ds.shuffle(10000)
+
+    # returns the next batch of data
+    features, labels = ds.make_one_shot_iterator().get_next()
+    return features, labels
+
+# training linear regression model
+
+
+def train_model(learning_rate, steps, batch_size, feature_columns, training_examples, training_targets,
+                validation_examples, validation_targets):
+
+    periods = 10
+    steps_per_period = steps / periods
+
+    # create a linear regressor object
+    linear_regressor = tf.estimator.LinearRegressor(
+        feature_columns=feature_columns, optimizer='SGD')
+
+    def training_input_fn(): return my_input_fn(training_examples, training_targets['CPUTime'],
+                                                batch_size=batch_size)
+
+    def predict_training_input_fn(): return my_input_fn(training_examples, training_targets['CPUTime'],
+                                                        num_epochs=1, shuffle=False)
+
+    def predict_validation_input_fn(): return my_input_fn(validation_examples, validation_targets['CPUTime'],
+                                                          num_epochs=1, shuffle=False)
+
+    # train the model but do so inside a loop so that we can periodically assess loss metrics
+    print('Training model...')
+    print('RMSE (on training data):')
+    training_rmse = []
+    validation_rmse = []
+
+    for period in range(0, periods):
+        # train the model, starting from prior state
+        linear_regressor.train(
+            input_fn=training_input_fn, steps=steps_per_period)
+
+        # take a break and compute predictions
+        training_predictions = linear_regressor.predict(
+            input_fn=predict_training_input_fn)
+        training_predictions = np.array(
+            [item['predictions'][0] for item in training_predictions])
+
+        validation_predictions = linear_regressor.predict(
+            input_fn=predict_validation_input_fn)
+        validation_predictions = np.array(
+            [item['predictions'][0] for item in validation_predictions])
+
+        # compute training and validation loss
+
+        training_root_mean_squared_error = math.sqrt(
+            metrics.mean_squared_error(training_predictions, training_targets))
+        validation_root_mean_squared_error = math.sqrt(
+            metrics.mean_squared_error(validation_predictions, validation_targets))
+
+        # occasionally print the current loss
+        print(' period %02d: %0.2f' %
+              (period, training_root_mean_squared_error))
+
+        # add the loss metrics from this period to our list
+        training_rmse.append(training_root_mean_squared_error)
+        validation_rmse.append(validation_root_mean_squared_error)
+    print('Model training finished')
+
+    # output to a graph of loss metris over periods
+    plt.ylabel('RMSE')
+    plt.xlabel('Periods')
+    plt.title('Root Mean Squared Error vs. Periods')
+    plt.tight_layout()
+    plt.plot(training_rmse, label='training')
+    plt.plot(validation_rmse, label='validation')
+    plt.ylim([0, 10000])
+    plt.legend()
+    plt.show()
+
+    return linear_regressor
+
+
+def test_model(learning_rate, steps, batch_size, feature_columns, testing_examples, testing_targets):
+
+    periods = 10
+    steps_per_period = steps / periods
+
+    # create a linear regressor object
+    linear_regressor = tf.estimator.LinearRegressor(
+        feature_columns=feature_columns, optimizer='Ftrl')
+
+    def testing_input_fn(): return my_input_fn(testing_examples, testing_targets['CPUTime'],
+                                               batch_size=batch_size)
+
+    def predict_testing_input_fn(): return my_input_fn(testing_examples, testing_targets['CPUTime'],
+                                                       num_epochs=1, shuffle=False)
+
+    # test the model
+    print('Testimg model...')
+    print('RMSE (on testing data):')
+    testing_rmse = []
+
+    for period in range(0, periods):
+        # take a break and compute predictions
+        testing_predictions = linear_regressor.predict(
+            input_fn=predict_testing_input_fn)
+        testing_predictions = np.array(
+            [item['predictions'][0] for item in testing_predictions])
+
+        # compute testing loss
+        testing_root_mean_squared_error = math.sqrt(
+            metrics.mean_squared_error(testing_predictions, testing_targets))
+
+        # occasionally print the current loss
+        print(' period %02d: %0.2f' %
+              (period, testing_root_mean_squared_error))
+
+        # add the loss metrics from this period to our list
+        testing_rmse.append(testing_root_mean_squared_error)
+    print('Model testing finished')
+
+    # output to a graph of loss metris over periods
+    plt.ylabel('RMSE')
+    plt.xlabel('Periods')
+    plt.title('Root Mean Squared Error vs. Periods')
+    plt.tight_layout()
+    plt.plot(testing_rmse, label='testing')
+    plt.ylim([0, 10000])
+    plt.legend()
+    plt.show()
+
+    return linear_regressor
+
+
+# main function
+if __name__ == '__main__':
+    # sort input data
+    InputData = pd.read_csv('InputData.csv', sep=',')
+    training_examples = InputData.head(300)
+    validation_examples = InputData.loc[301:400]
+    testing_examples = InputData.tail(100)
+
+    # sort target data
+    CPUTimes = pd.read_csv('CPUTimes.csv', sep=',')
+    CPUTimes = CPUTimes[["CPUTime"]]
+    training_targets = CPUTimes.head(300)
+    validation_targets = CPUTimes.loc[301:400]
+    testing_targets = CPUTimes.tail(100)
+
+    # visualize sorted input data
+    print('Training examples summary:')
+    display.display(training_examples.describe())
+    print('Validation examples summary:')
+    display.display(validation_examples.describe())
+    print('Testing examples summary:')
+    display.display(testing_examples.describe())
+
+    # visualize sorted target data
+    print('Training targets summary:')
+    display.display(training_targets.describe())
+    print('Validation targets summary:')
+    display.display(validation_targets.describe())
+    print('Testing targets summary:')
+    display.display(testing_targets.describe())
+
+    construct_feature_columns()
+    # train a linear regression model
+
+    train_model(
+        learning_rate=1.0,
+        steps=500,
+        batch_size=16,
+        feature_columns=construct_feature_columns(),
+        training_examples=training_examples,
+        training_targets=training_targets,
+        validation_examples=validation_examples,
+        validation_targets=validation_targets)
+
+    test_model(
+        learning_rate=1.0,
+        steps=500,
+        batch_size=16,
+        feature_columns=construct_feature_columns(),
+        testing_examples=testing_examples,
+        testing_targets=testing_targets)
---
+++ extractBuildInfo.py
@@ -0,0 +1,76 @@
+import os
+import csv
+
+rowList = [["ID", "prefix", "type"]]
+
+def getPrefix(gitDiff):
+    dict = {}
+    dict["bazelci/"] = gitDiff.count("bazelci/")
+    dict["examples/"] = gitDiff.count("examples/")
+    dict["scripts/"] = gitDiff.count("scripts/")
+    dict["site/"] = gitDiff.count("site/")
+    dict["src/conditions"] = gitDiff.count("src/conditions")
+    dict["src/java_tools"] = gitDiff.count("src/java_tools")
+    dict["src/main"] = gitDiff.count("src/main")
+    dict["src/test"] = gitDiff.count("src/test")
+    dict["src/tools"] = gitDiff.count("src/tools")
+    dict["third_party/"] = gitDiff.count("third_party/")
+    dict["tools/"] = gitDiff.count("tools/")
+    
+    # add selection between two equivalent counts
+    return max(dict, key=dict.get)
+    # no tie breaker
+
+def getType(gitDiff):
+    dict = {}
+    dict["JAVA"] = gitDiff.count(".java")
+    dict["C/C++"] = gitDiff.count(".c")
+    dict["C/C++"] += gitDiff.count(".h")
+    dict["C/C++"] += gitDiff.count(".cc")
+    dict["C/C++"] += gitDiff.count(".cpp")
+    dict["Starlark"] = gitDiff.count("BUILD")
+    dict["Starlark"] += gitDiff.count(".bazel")
+    dict["Starlark"] += gitDiff.count("WORKSPACE")
+    dict["Starlark"] += gitDiff.count("*.bzl")
+    dict["python"] = gitDiff.count(".py")
+    dict["HTML/CSS/JS"] = gitDiff.count(".html")
+    dict["HTML/CSS/JS"] += gitDiff.count(".css")
+    dict["HTML/CSS/JS"] += gitDiff.count(".js")
+    
+    # add other category for file types
+    return max(dict, key=dict.get)
+    # no other category
+
+def parseCommits(name):
+    # move to bazel project
+    os.chdir("/Users/muhammadumarnadeem/bazel")
+
+    # obtain information about last commit
+    os.system(f"git checkout HEAD~1")
+    id = os.popen("git rev-parse --short HEAD").read()
+    idLength = len(id)
+    id = id[1:idLength-1]
+    gitDiff = os.popen("git diff HEAD~1..HEAD --name-only").read()
+    prefix = getPrefix(gitDiff)
+    type = getType(gitDiff)
+
+    # append commit information to list
+    rowList.append([id, prefix, type])
+
+    # pull JSON files from bazel build info
+    os.system(f"bazel build //src:bazel-dev --build_event_json_file=/Users/muhammadumarnadeem/CS229-Final-Project/Build_Info/{name}_commit.json")
+    
+    # move back to local project
+    os.chdir("/Users/muhammadumarnadeem/CS229-Final-Project")
+
+
+if __name__ == '__main__':
+    # extract JSON files (for CPUTimes) and InputData from Commits
+    numCommits = 501
+    for i in range(1, numCommits):
+        parseCommits(i)
+    
+    # Write InputData into csv file
+    with open('InputData.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(rowList)
